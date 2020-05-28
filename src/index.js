7c40cf5b150bd033eaa0bfc17fb366f2be59c362
laoqin <694170828@qq.com>
2020-05-28 09:32:20 +0800

    处理如果参数传入的是对象

--- src/index.js
+++ src/index.js
@@ -21,17 +21,33 @@ function reducer(state = { num: 1 }, action) {
 
 let store = createStore(reducer)
 
-let add = () => ({
-  type: ADD,
-})
-let cut = (p) => ({
-  type: CUT,
-  num: p,
-})
+// 如果是函数
+// let add = () => ({
+//   type: ADD,
+// })
+// let cut = (p) => ({
+//   type: CUT,
+//   num: p,
+// })
+// // 让dispatch 的派发动作让函数内部帮我实现
+// add = bindActionCreators(add, store.dispatch)
+// cut = bindActionCreators(cut, store.dispatch)
 
-add = bindActionCreators(add, store.dispatch)
-
-cut = bindActionCreators(cut, store.dispatch)
+// 如果是对象
+let actions = {
+  add() {
+    return {
+      type: ADD,
+    }
+  },
+  cut(p) {
+    return {
+      type: CUT,
+      num: p,
+    }
+  },
+}
+actions = bindActionCreators(actions, store.dispatch)
 
 class App extends Component {
   constructor(props) {
@@ -53,8 +69,8 @@ class App extends Component {
     return (
       <div>
         <p>{this.state.num}</p>
-        <button onClick={add}>+</button>
-        <button onClick={() => cut(3)}>-</button>
+        <button onClick={actions.add}>+</button>
+        <button onClick={() => actions.cut(3)}>-</button>
       </div>
     )
   }
--- src/redux/bindActionCreators.js
+++ src/redux/bindActionCreators.js
@@ -1,8 +1,34 @@
+/**
+ * 将action进行绑定， 主要为了简化语法
+ * @param {*} actionCreators
+ * @param {*} dispatch
+ */
 export default function bindActionCreators(actionCreators, dispatch) {
   function bindActionCreator(actionCreator, dispatch) {
     return (...args) => dispatch(actionCreator(...args))
   }
+  // 判断bindActionCreators 参数可能是函数， 也可能是对象
   if (typeof actionCreators === 'function') {
     return bindActionCreator(actionCreators, dispatch)
   }
+  // 如果是对象
+  let boundActionCreators = {}
+  // 循环这个对象
+  for (const key in actionCreators) {
+    boundActionCreators[key] = bindActionCreator(actionCreators[key], dispatch)
+  }
+  return boundActionCreators
 }
+
+// 传函数
+/**
+  let add = () => ({
+    type: ADD,
+  })
+  let cut = (p) => ({
+    type: CUT,
+    num: p,
+  })
+  add = bindActionCreators(add, store.dispatch)
+  cut = bindActionCreators(cut, store.dispatch)
+ */
