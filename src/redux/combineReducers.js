e8c54d93da8cf1c3755a21b58c75df3be26f1e7c
laoqin <694170828@qq.com>
2020-05-28 12:46:25 +0800

    实现 combineReducers 函数

--- src/redux/combineReducers.js
+++ src/redux/combineReducers.js
@@ -1,2 +1,16 @@
 // 合并多个reducer 函数
-export default function combineReducers() {}
+export default function combineReducers(reducers) {
+  let reducerKeys = Object.keys(reducers)
+  return function (state = {}, action) {
+    let hasChanged = false // 此次派发action是否引起了状态的修改， 或者说状态的改变
+    const nextState = {}
+    for (let i = 0; i < reducerKeys.length; i++) {
+      const key = reducerKeys[i]
+      const previousStateForKey = state[key]
+      let nextStateForKey = reducers[key](previousStateForKey, action)
+      nextState[key] = nextStateForKey
+      hasChanged = hasChanged || nextStateForKey !== previousStateForKey
+    }
+    return hasChanged ? nextState : state
+  }
+}
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,16 @@
 import a from './a'
 import b from './b'
 
+import { combineReducers } from './../../redux'
+
 // 合并a, b 两个reducer
-export default function (state = {}, action) {
-  let newState = {}
-  newState.a = a(state.a, action)
-  newState.b = b(state.b, action)
-  return newState
-}
+// export default function (state = {}, action) {
+//   let newState = {}
+//   newState.a = a(state.a, action)
+//   newState.b = b(state.b, action)
+//   return newState
+// }
+export default combineReducers({
+  a,
+  b,
+})
