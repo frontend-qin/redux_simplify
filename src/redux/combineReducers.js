5c92a567b197d260d61c170de78aa197c24e236b
laoqin <694170828@qq.com>
2020-05-29 09:31:11 +0800

    add 注释

--- src/redux/combineReducers.js
+++ src/redux/combineReducers.js
@@ -1,16 +1,24 @@
 // 合并多个reducer 函数
 export default function combineReducers(reducers) {
-  let reducerKeys = Object.keys(reducers)
+  // 拿到所有的对象的key 组成的数组
+  let reducerKeys = Object.keys(reducers);
+  // 返回一个函数
   return function (state = {}, action) {
-    let hasChanged = false // 此次派发action是否引起了状态的修改， 或者说状态的改变
-    const nextState = {}
+    // 此次派发action是否引起了状态的修改， 或者说状态的改变
+    let hasChanged = false;
+    // 定义一个空对象
+    const nextState = {};
+    // 循环所有的key组成的那个数组
     for (let i = 0; i < reducerKeys.length; i++) {
-      const key = reducerKeys[i]
-      const previousStateForKey = state[key]
-      let nextStateForKey = reducers[key](previousStateForKey, action)
-      nextState[key] = nextStateForKey
-      hasChanged = hasChanged || nextStateForKey !== previousStateForKey
+      // 拿到每一个key
+      const key = reducerKeys[i];
+      // 拿到老的状态对象的值
+      const previousStateForKey = state[key];
+      // 调用这个 `reducers[key]` 的值是个函数
+      let nextStateForKey = reducers[key](previousStateForKey, action);
+      nextState[key] = nextStateForKey;
+      hasChanged = hasChanged || nextStateForKey !== previousStateForKey;
     }
-    return hasChanged ? nextState : state
-  }
+    return hasChanged ? nextState : state;
+  };
 }
